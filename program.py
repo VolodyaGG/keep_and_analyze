4426336861e60dd1edad8c64c01f1a9963f34f59
Vladimir <vovalaptev468@gmail.com>
2025-07-23 22:39:20 +0300

    splitter and logo

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QFrame, QVBoxLayout, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QFrame, QVBoxLayout, QHBoxLayout, QLabel, QSizePolicy
 import sys
-from PySide6.QtGui import QCloseEvent, QScreen
+from PySide6.QtGui import QCloseEvent, QScreen, QPixmap
+from PySide6.QtCore import Qt
 
 
 class ExampleWindow(QWidget):
@@ -15,22 +16,23 @@ class ExampleWindow(QWidget):
         self.show()
 
     def setup(self):
-        self.setStyleSheet("background-color: #D3B8D8;")
+        #the bottom layer
+        main_layout = QHBoxLayout(self) 
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
 
-        layout = QHBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(0)
-
+        #left panel
         self.left_panel = self.createLeftPanel()
-        layout.addWidget(self.left_panel, 3)
+        main_layout.addWidget(self.left_panel, 3)
 
-        content = QWidget()
-        content_layout = QVBoxLayout(content)
+        splitter = self.createLeftSPlitter()
+        main_layout.addWidget(splitter)
 
-        content_layout.addStretch()
+        #content part
+        self.content_part = self.createContent()
+        main_layout.addWidget(self.content_part, 7)
 
-        layout.addWidget(content, 7)
-
+        #center location
         posX = (self.ScreenSize.width() - self.width)/2
         posY = (self.ScreenSize.height() - self.height)/2
 
@@ -42,9 +44,42 @@ class ExampleWindow(QWidget):
         frame.setStyleSheet("background-color: #E0A4A4;")
 
         layout = QVBoxLayout(frame)
+        image_label = QLabel()
+        pixmap = QPixmap("images/logo.png")
+        image_label.setPixmap(pixmap)
+        image_label.setScaledContents(False)
+        image_label.setAlignment(Qt.AlignCenter)
+
+        image_label.setMinimumSize(100,100)
+        image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        image_label.setPixmap(pixmap.scaled(image_label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+        def resizeEvent(event):
+            size = min(image_label.width(), image_label.height())
+            image_label.setFixedSize(size,size)
+            image_label.setPixmap(pixmap.scaled(size,size,Qt.KeepAspectRatio, Qt.SmoothTransformation))
+            event.accept()
+
+        image_label.resizeEvent = resizeEvent
+        
+        layout.addWidget(image_label)
         layout.addStretch()
         return frame
+    
+    def createContent(self):
+        content = QFrame()
+        content.setStyleSheet("background-color: #D3B8D8;")
 
+        layout = QVBoxLayout(content)
+        layout.addStretch()
+        return content
+        
+    def createLeftSPlitter(self):
+        frame = QFrame()
+        frame.setStyleSheet("background-color: white;")
+        frame.setFixedWidth(5)
+        return frame
 
     def closeEvent(self, event: QCloseEvent):
         reply = QMessageBox.question(
